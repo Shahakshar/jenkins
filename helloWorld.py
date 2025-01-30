fc41d71461363e7744da9cfcf2c4d4e04b0a51e5
Shah Akshar <72061051+Shahakshar@users.noreply.github.com>
2025-01-30 16:30:44 +0530

    Update helloWorld.py

--- helloWorld.py
+++ helloWorld.py
@@ -2,5 +2,5 @@ print("this is me akshar")
 print("From Gujarat")
 print("I am Engineer")
 print("configure everything")
-print("we are successfully implement the webhook and integrate with the jenkins")
+
 ## done with eamil integration if any failure occure
